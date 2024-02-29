b829236b2f55e7d3f5d3bee3632027982ae4aa45
Gregory Levit <levitg@gmail.com>
2024-02-29 15:19:08 +0300

    create

---
+++ tutorial-cypress/applitools.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testConcurrency: 4,
+  //
+  browser: [
+    // Add browsers with different viewports
+    {width: 1000, height: 660, name: 'chrome'},
+    //{width: 700, height: 500, name: 'firefox'},
+    //{width: 1600, height: 1200, name: 'ie11'},
+    //{width: 1024, height: 768, name: 'edgechromium'},
+    //{width: 800, height: 600, name: 'safari'} ,
+    // Add mobile emulation devices in Portrait mode
+    //{deviceName: 'iPhone X', screenOrientation: 'portrait'},
+    //{deviceName: 'Pixel 2', screenOrientation: 'portrait'}
+  ],
+  // set batch name to the configuration
+  //batchName: 'Demo',
+  //branchName: 'bmurmistro/GithubIntegrationCypress/master',
+  //serverUrl: 'https://testeyesapi.applitools.com',
+  batchId: process.env.APPLITOOLS_BATCH_ID,
+  failCypressOnDiff: false,
+  dontCloseBatches: true
+}
---
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -0,0 +1,18 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+});
+describe('Hello Cypress, This is Applitools', () => {
+  it('works', () => {
+    cy.visit('https://applitools.com/helloworld')
+    cy.eyesOpen({
+      appName: 'Walmart Feb25 Demo',
+      testName: 'Hello'
+    });
+    //cy.contains('?diff2').click();
+    //cy.contains('Click me!').click();
+    cy.eyesCheckWindow('Hello');
+    cy.eyesClose();
+  });
+});
